12e63480c2a91f5f8a9198f7c6518245662cfe22
MDSABIULLAH <MDSABIULLAH321@GMAIL.COM>
2025-07-20 20:21:45 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <Button className="bg-blue-500 text-white hover:bg-blue-600">
+    Click Me
+    </Button>
+  );
+};
